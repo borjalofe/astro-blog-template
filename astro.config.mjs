e50c296df7fce11829f692134425762d1a752aae
Borja LoFe <im@borjalofe.com>
2024-08-30 19:47:55 +0200

    feat: :heavy_plus_sign: Integrate React for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 
 import { SITE_URL as site } from './src/consts';
 
+
 // https://astro.build/config
 export default defineConfig({
-	site,
-	integrations: [mdx(), sitemap()],
+  site,
+  integrations: [mdx(), react(), sitemap()]
 });
