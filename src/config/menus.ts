21d3e8b28825b8db108fb956d558ecc201ecbc1b
Borja LoFe <im@borjalofe.com>
2024-09-04 15:51:54 +0200

    fix: :bug: Add missing about menu item

--- src/config/menus.ts
+++ src/config/menus.ts
@@ -3,6 +3,7 @@ import GithubIcon from '@/components/icons/GithubIcon';
 
 export const mainMenu: Menu = [
     { label: 'Home', path: '/' },
+    { label: 'About Me', path: '/about' },
     { label: 'Blog', path: '/blog' },
 ];
 
