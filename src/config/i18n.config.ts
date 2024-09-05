6d75c179c4fd782df311958858c0a2aae4370ae4
Borja LoFe <im@borjalofe.com>
2024-09-05 14:56:25 +0200

    refactor: :fire: Remove unneeded literals

--- src/config/i18n.config.ts
+++ src/config/i18n.config.ts
@@ -1,4 +1,2 @@
 export const defaultLocale: string = "en";
 export const locales = ["en", "es"];
-export const localesBasePath = 'locales';
-export const localizedDateOptionsFile = 'date.options.ts';
